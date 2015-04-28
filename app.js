5e935dd589d18727217cf4b301befad1e1634ba1
zachbradshaw <bradshaw.zachary@gmail.com>
2015-04-28 16:53:58 -0400

    testing gh-pages deploy

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-angular.module('fakeCo', []);
+var app = angular.module('fakeCo', []);
--- js/appt-controller.js
+++ js/appt-controller.js
@@ -1,12 +1,22 @@
-angular.module('fakeCo', [])
-.controller('apptController', function ($http) {
+app.controller('apptController', function ($http, $scope) {
 
   var getAppts = {
     method: 'GET',
     url: 'http://fake-co-calendar.herokuapp.com/api/v1/events'
   }
 
+  $scope.meetingRooms = ['Main Conference Room',
+                        'Room A',
+                        'Room B',
+                        'Room C',
+                        'Room D',
+                        'Room E',
+                        'Room F',
+                        'Room G',
+                        'Room H']
+
   $http(getAppts).success(function (d) {
-    console.log(d);
+    $scope.appts = d.events.list;
+    console.log($scope.appts);
   })
 });
