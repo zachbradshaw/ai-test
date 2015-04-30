7b59d97259d804d790b48fe0b7fbc1586df14e84
zachbradshaw <bradshaw.zachary@gmail.com>
2015-04-30 17:42:02 -0400

    working on formatting times

--- js/appt-controller.js
+++ js/appt-controller.js
@@ -12,25 +12,46 @@ app.controller('apptController', function ($http, $scope) {
   $scope.currentTime = function() {
     var today = new Date();
     var h = today.getHours();
+    var afternoon = h - 12;
     var m = today.getMinutes();
-    
     m = checkTime(m);
 
-    // if (h > 12) {
-    //   h = h - 12;
-    // }
+    if (h > 12) {
+      h = afternoon;
+    }
     document.querySelector('.current-time').innerHTML = 'Current Time: ' + '<span class="current-hour">' + h + '</span>' + ":" + '<span class="current-minute">' + m + '<span>';
     t = setTimeout(function () {
         $scope.currentTime();
     }, 500);
   }
-  
+
   $http.jsonp(url).success(function (d) {
 
     for (var i = 0; i < d.events.list.length; ++i) {
       d.events.list[i].date = d.events.list[i].start_time.slice(0, 10);
-      d.events.list[i].short_start_time = d.events.list[i].start_time.slice(11, 16);
-      d.events.list[i].short_end_time = d.events.list[i].end_time.slice(11, 16);
+      d.events.list[i].start_hour = Number(d.events.list[i].start_time.slice(11, 13));
+      console.log(d.events.list[i].start_hour);
+      d.events.list[i].start_minutes = Number(d.events.list[i].start_time.slice(14, 16));
+      d.events.list[i].end_hour = Number(d.events.list[i].end_time.slice(11, 13));
+      d.events.list[i].end_minutes = Number(d.events.list[i].end_time.slice(14, 16));
+
+      if (d.events.list[i].start_hour < 10) {
+        d.events.list[i].start_hour = d.events.list[i].start_time.slice(12, 13);
+      }
+
+      if (d.events.list[i].end_hour < 10) {
+        d.events.list[i].end_hour = d.events.list[i].end_time.slice(12, 13);
+      }
+
+      if (d.events.list[i].start_minutes === 0) {
+        d.events.list[i].start_minutes = '00';
+      }
+
+      if (d.events.list[i].end_minutes === 0) {
+        d.events.list[i].end_minutes = '00';
+      }
+      // d.events.list[i].short_start_time = d.events.list[i].start_time.slice(11, 16);
+      // d.events.list[i].short_end_time = d.events.list[i].end_time.slice(11, 16);
     }
     $scope.appts = d.events.list;
     console.log($scope.appts);
@@ -41,17 +62,19 @@ app.controller('apptController', function ($http, $scope) {
     restrict: 'E',
     link: function (scope, elem, attrs) {
       var currentHour;
+      var newCurrentHour;
       var currentMinute;
+      var startHour;
+      var startMinutes;
+      var endHour;
+      var endMinutes;
       setTimeout(function () {
-        currentHour = Number($('.current-hour').text());
         currentMinute = Number($('.current-minute').text());
+        startHour = Number($('.start-hour').text());
+        startMinutes = Number($('.start-minutes').text());
+        endHour = Number($('.end-hour').text());
+        endMinutes = Number($('.end-minutes').text());
 
-        if (currentHour > 12) {
-          $(currentHour).text(currentHour - 12);
-        }
-
-        console.log(currentHour);
-        console.log(currentMinute);
       }, 500)
     }
   }
